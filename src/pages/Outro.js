db34e9dbf0d4a5516a43c1c9c580e6119d441d92
leefnek <rur.rur.xp@gmail.com>
2022-11-22 11:04:36 +0900

    feat: Outro page

--- src/pages/Outro.js
+++ src/pages/Outro.js
@@ -1,5 +1,17 @@
 class Outro {
-  constructor() {}
+  constructor($App) {
+    this.$Outro = $App.querySelector("#Outro");
+    this.$Outro.style.display = "none";
+    $App.addEventListener("click", () => {
+      switch (getStep()) {
+        case 4:
+          this.$Outro.style.display = "none";
+          break;
+        case 3:
+          this.$Outro.style.display = "flex";
+      }
+    });
+  }
 }
 
 export default Outro;
